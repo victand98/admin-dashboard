1341b7df894c59c68236a68c340439759c55aab4
Víctor Andrés Rojas <victandres98@gmail.com>
2021-05-06 23:09:16 -0500

    404 Page

--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: [
-  ]
+  styleUrls: ['./no-page-found.component.css'],
 })
-export class NoPageFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NoPageFoundComponent {
+  year = new Date().getFullYear();
 }
