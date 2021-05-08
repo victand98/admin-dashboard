51534b77c3ad2d9baf1478e589d249aa68711d5d
Víctor Andrés Rojas <victandres98@gmail.com>
2021-05-08 13:43:40 -0500

    Account Settings: Creating Service

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,19 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SettingsService } from '../services/settings.service';
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: [],
 })
 export class PagesComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
+  constructor(private settingsService: SettingsService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {
-    const url =
-      localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
-
-    this.linkTheme?.setAttribute('href', url);
-  }
+  ngOnInit(): void {}
 }
