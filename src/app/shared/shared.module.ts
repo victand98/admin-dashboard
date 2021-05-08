f29b156db97e903966643371537faebc819da60b
Víctor Andrés Rojas <victandres98@gmail.com>
2021-05-08 13:50:19 -0500

    routerLink property

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,6 +9,6 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [BreadcrumbsComponent, SidebarComponent, HeaderComponent],
   exports: [BreadcrumbsComponent, SidebarComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
