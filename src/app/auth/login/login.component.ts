7cc25935ade70b0db3a81d7471eef6faf3ec5732
Víctor Andrés Rojas <victandres98@gmail.com>
2021-05-16 12:37:35 -0500

    Google SignIn

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 
 import { UserService } from 'src/app/services/user.service';
 
+declare const gapi: any;
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public auth2: any;
+
   public loginForm = this.fb.group({
-    email: ['victandres98@gmail.com', [Validators.required, Validators.email]],
+    email: [
+      localStorage.getItem('email') || 'victandres98@gmail.com',
+      [Validators.required, Validators.email],
+    ],
     password: ['123456', Validators.required],
     remember: [false],
   });
@@ -20,13 +27,66 @@ export class LoginComponent {
   constructor(
     private router: Router,
     private fb: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private ngZone: NgZone
   ) {}
+
+  ngOnInit(): void {
+    this.renderButton();
+  }
 
   login() {
     this.userService.login(this.loginForm.value).subscribe(
-      (res) => console.log(`res`, res),
+      (res) => {
+        if (this.loginForm.get('remember')?.value) {
+          localStorage.setItem('email', this.loginForm.get('email')?.value);
+        } else {
+          localStorage.removeItem('email');
+        }
+
+        // Navegar al Dashboard
+        this.router.navigateByUrl('/');
+      },
       (err) => Swal.fire('Error', err.error.msg, 'error')
+    );
+  }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+    });
+
+    this.startApp();
+  }
+
+  async startApp() {
+    await this.userService.googleInit();
+    this.auth2 = this.userService.auth2;
+
+    this.attachSignin(document.getElementById('my-signin2'));
+  }
+
+  attachSignin(element: any) {
+    this.auth2.attachClickHandler(
+      element,
+      {},
+      (googleUser: any) => {
+        const id_token = googleUser.getAuthResponse().id_token;
+        // console.log(id_token);
+        this.userService.loginGoogle(id_token).subscribe((resp) => {
+          // Navegar al Dashboard
+          this.ngZone.run(() => {
+            this.router.navigateByUrl('/');
+          });
+        });
+      },
+      (error: any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      }
     );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,23 +1,84 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 import { LoginForm } from '../interfaces/login-form.interface';
 import { RegisterForm } from '../interfaces/register-form.interface';
 
 const base_url = environment.base_url;
+declare const gapi: any;
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  public auth2: any;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private ngZone: NgZone
+  ) {
+    this.googleInit();
+  }
+
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
+
+  googleInit() {
+    return new Promise((resolve) => {
+      gapi.load('auth2', () => {
+        this.auth2 = gapi.auth2.init({
+          client_id:
+            '1045072534136-oqkjcjvo449uls0bttgvl3aejelh22f5.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin',
+        });
+
+        resolve('');
+      });
+    });
+  }
 
   createUser(formData: RegisterForm) {
-    return this.http.post(`${base_url}/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData).pipe(
+      tap((resp: any) => {
+        this.saveOnLocalStorage(resp.token, resp.menu);
+      })
+    );
   }
 
   login(formData: LoginForm) {
-    return this.http.post(`${base_url}/login`, formData);
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        this.saveOnLocalStorage(resp.token, resp.menu);
+      })
+    );
+  }
+
+  saveOnLocalStorage(token: string, menu: any) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('menu', JSON.stringify(menu));
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('menu');
+
+    this.auth2.signOut().then(() => {
+      this.ngZone.run(() => {
+        this.router.navigateByUrl('/login');
+      });
+    });
+  }
+
+  loginGoogle(token: string) {
+    return this.http.post(`${base_url}/login/google`, { token }).pipe(
+      tap((resp: any) => {
+        this.saveOnLocalStorage(resp.token, resp.menu);
+      })
+    );
   }
 }
